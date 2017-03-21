13807a939608ba748f036834ee06155e3877144e
adamzfc <adamzfc@gmail.com>
2017-03-21 09:24:30 +0800

    update smartpointer v3

--- smartpointer/smartpointer.h
+++ smartpointer/smartpointer.h
@@ -11,23 +11,42 @@ public:
     };
     SmartPointer(T *p):mPointer(p) {
         std::cout <<"create smart pointer at "<<static_cast<const void*>(p)<<std::endl;
+        if (mPointer) mPointer->incRefCount();
     };
     ~SmartPointer(){
         std::cout << "release smart pointer at "<<static_cast<const void*>(mPointer)<<std::endl;
-        if (mPointer) delete mPointer;
+        if (mPointer && mPointer->decRefCount() == 0) delete mPointer;
     };
     SmartPointer(const SmartPointer &other):mPointer(other.mPointer) {
         std::cout << "Copy smart pointer at " << static_cast<const void*>(other.mPointer) << std::endl;
+        if (mPointer) mPointer->incRefCount();
     };
     SmartPointer &operator = (const SmartPointer &other) {
-        if (this == &other) return *this;
-        if (mPointer) delete mPointer;
-        mPointer = other.mPointer;
-        std::cout << "Assign smart pointer at " << static_cast<const void*>(other.mPointer) << std::endl;
+        T *temp(other.mPointer);
+        if (temp) temp->incRefCount();
+        if (mPointer && mPointer->decRefCount() == 0) delete mPointer;
+        mPointer = temp;
         return *this;
     };
 private:
     T *mPointer;
+};
+
+class RefBase {
+public:
+    RefBase() : mCount(0) {}
+    void incRefCount() {
+        mCount ++;
+    }
+    int decRefCount() {
+        return --mCount;
+    }
+    int getRefCount() {
+        return mCount;
+    }
+    virtual ~RefBase() {}
+private:
+    int mCount;
 };
 
 #endif
--- smartpointer/test.cpp
+++ smartpointer/test.cpp
@@ -1,20 +1,23 @@
 #include "smartpointer.h"
 
-class SomeClass {
+class SomeClass : public RefBase {
 public:
     SomeClass() {
         std::cout << "SomeClass default constructor !" << std::endl;
     }
+    ~SomeClass() {
+        std::cout << "SomeClass deconstructor !" << std::endl;
+    }
 };
 
-void testcase1() {
+/*void testcase1() {
     SmartPointer<char> spunknown;
     SmartPointer<char> spnull = NULL;
     SmartPointer<SomeClass> spclass = new SomeClass;
 //    SmartPointer<const char> spstr = "Hello world!";
-}
+}*/
 
-void testcase2() {
+/*void testcase2() {
     SmartPointer<SomeClass> spclass1 = new SomeClass;
     std::cout << std::endl;
     spclass1 = spclass1;
@@ -25,10 +28,25 @@ void testcase2() {
     std::cout << std::endl;
     spclass2 = spclass1;
     std::cout << std::endl;
+}*/
+
+void testcase3() {
+    SomeClass *pSomeClass = new SomeClass();
+    SmartPointer<SomeClass> spOuter = pSomeClass;
+    std::cout << "SomeClass Ref Count (" << pSomeClass->getRefCount() << ") outer 1." << std::endl;
+    {
+        SmartPointer<SomeClass> spInner = spOuter;
+        std::cout << "SomeClass Ref Count (" << pSomeClass->getRefCount() << ") inner." << std::endl;
+    }
+    std::cout << "SomeClass Ref Count (" << pSomeClass->getRefCount() << ") outer 2." << std::endl;
+    std::cout << "new another SomeClass class for spOuter." << std::endl;
+    SmartPointer<SomeClass> spOuter2 = new SomeClass();
+    spOuter = spOuter2;
 }
 
 int main() {
-    testcase1();
-    testcase2();
+//    testcase1();
+//    testcase2();
+    testcase3();
     return 0;
 }
