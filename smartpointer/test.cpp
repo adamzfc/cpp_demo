79b229a4ac67ea2d2f648de5590b579129b6b619
adamzfc <adamzfc@gmail.com>
2017-03-21 09:07:05 +0800

    update smartpointer v2

--- smartpointer/test.cpp
+++ smartpointer/test.cpp
@@ -7,14 +7,28 @@ public:
     }
 };
 
-void testcast1() {
+void testcase1() {
     SmartPointer<char> spunknown;
     SmartPointer<char> spnull = NULL;
     SmartPointer<SomeClass> spclass = new SomeClass;
 //    SmartPointer<const char> spstr = "Hello world!";
 }
 
+void testcase2() {
+    SmartPointer<SomeClass> spclass1 = new SomeClass;
+    std::cout << std::endl;
+    spclass1 = spclass1;
+    std::cout << std::endl;
+    SmartPointer<SomeClass> spclassother = spclass1;
+    std::cout << std::endl;
+    SmartPointer<SomeClass> spclass2 = new SomeClass;
+    std::cout << std::endl;
+    spclass2 = spclass1;
+    std::cout << std::endl;
+}
+
 int main() {
-    testcast1();
+    testcase1();
+    testcase2();
     return 0;
 }
